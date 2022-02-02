87050b81e8371b07894a7e6b7b0f36ff0256482d
Kitt Cheung <kitt.cheung@se.experis.com>
2022-02-02 13:30:41 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login-components/login.component';
 
-@NgModule({
+@NgModule({ //Decorator
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule
--- src/app/login-components/login.component.ts
+++ src/app/login-components/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:"app-login",
+    templateUrl:"./login.component.html",
+    styleUrls:["./login.component.css"],
 }) //Decorator
 export class LoginComponent{
 
