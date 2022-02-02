5b0825f7bf22437bc85118276d210ade4f08afa0
Kitt Cheung <kitt.cheung@se.experis.com>
2022-02-02 14:30:26 +0100

    feat: fetch user/ trainer from api

--- src/app/login-components/login.component.ts
+++ src/app/login-components/login.component.ts
@@ -1,10 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { UsersService } from "../services/Users.service";
 
 @Component({
     selector:"app-login",
     templateUrl:"./login.component.html",
     styleUrls:["./login.component.css"],
 }) //Decorator
-export class LoginComponent{
+export class LoginComponent implements OnInit{
+constructor(private readonly UserService: UsersService){
 
+}
+ngOnInit(): void {
+    this.UserService.fetchUsers();
+}
 }
--- src/app/services/Users.service.ts
+++ src/app/services/Users.service.ts
@@ -1,24 +1,24 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import {User} from "../models/user.model";
+import { User } from "../models/user.model";
 
 @Injectable({
-providedIn:"root"
+    providedIn: "root"
 })
 
-export class UsersService{
-    private users: User[]= [];
-    private error:string = "";
-    
-    constructor(private readonly http:HttpClient){
+export class UsersService {
+    private users: User[] = [];
+    private error: string = "";
+
+    constructor(private readonly http: HttpClient) {
     }
 
-    public fetchUsers():void {
+    public fetchUsers(): void {
         this.http.get<User[]>("https://trivia-game-noroff-api.herokuapp.com/trainers")
-        .subscribe((users : User[])=> {
-            this.users=users;
-        },(error:HttpErrorResponse)=>{
-            this.error= error.message;
-        })
+            .subscribe((users: User[]) => {
+                this.users = users;
+            }, (error: HttpErrorResponse) => {
+                this.error = error.message;
+            })
     }
 }
